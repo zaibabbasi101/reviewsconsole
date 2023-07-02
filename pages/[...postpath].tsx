d6f17fc2bdd34adc5a82a9854ee85d4675b4edf1
zaibabbasi101 <126961171+zaibabbasi101@users.noreply.github.com>
2023-07-02 16:26:10 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -5,7 +5,7 @@ import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
-	const graphQLClient = "https://reviewsconsole.com/graphql"
+	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
 	const path = pathArr.join('/');
